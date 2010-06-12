32ea49922a63142adf96f21ccdb6a82caa2efc4c
fraer72 <fraer72@gmail.com>
2010-06-12 12:04:41 +0600

    ? utf

--- Script/messages.java
+++ Script/messages.java
@@ -1,29 +1,29 @@
-
+п»ї
 import java.util.StringTokenizer;
 import ru.jimbot.util.*;
 
  /*
   * @author fraer72
-  * Антиреклама
+  * РђРЅС‚РёСЂРµРєР»Р°РјР°
   */
 
   try {
-  Integer maxCnt = 3; // Максимально число цифр в сообщении
+  Integer maxCnt = 3; // РњР°РєСЃРёРјР°Р»СЊРЅРѕ С‡РёСЃР»Рѕ С†РёС„СЂ РІ СЃРѕРѕР±С‰РµРЅРёРё
   Integer Cnt = 0;
   char[] number_0 = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-  String[] number_1 = {"ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+  String[] number_1 = {"РЅРѕР»СЊ", "РѕРґРёРЅ", "РґРІР°", "С‚СЂРё", "С‡РµС‚С‹СЂРµ", "РїСЏС‚СЊ", "С€РµСЃС‚СЊ", "СЃРµРјСЊ", "РІРѕСЃРµРјСЊ", "РґРµРІСЏС‚СЊ"};
   String delimiters = " \t\n\r,.";
 
   StringTokenizer st = new StringTokenizer(msg, delimiters);
 
-   while(st.hasMoreTokens()){  // Перебираем сообщение
+   while(st.hasMoreTokens()){  // РџРµСЂРµР±РёСЂР°РµРј СЃРѕРѕР±С‰РµРЅРёРµ
    String s = st.nextToken();
      for (int i = 0 ;i < number_0.length; i++){
 	   if(s.indexOf(number_0[i]) != -1 ||
                s.indexOf(number_1[i]) != -1) 
 			   Cnt++;
 	     if(Cnt > maxCnt) 
-		 msg = msg.replace(s, "*"); // Если выше максимального, закроем
+		 msg = msg.replace(s, "*"); // Р•СЃР»Рё РІС‹С€Рµ РјР°РєСЃРёРјР°Р»СЊРЅРѕРіРѕ, Р·Р°РєСЂРѕРµРј
 	 }
    }
 
